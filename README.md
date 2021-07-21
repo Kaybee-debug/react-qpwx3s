ee43760f5fc6f08154157a82adb2d47393222d2a
Kaybee-debug <karabomolepo29@gmail.com>
2021-07-21 22:28:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-qpwx3s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qpwx3s)
---
+++ src/App.js
@@ -0,0 +1,308 @@
+import React from 'react';
+import './style.css';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import ToDoList from './Pages/ToDoList';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+//components
+//import {Nav} from 'components';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useHistory,
+  useLocation
+} from 'react-router-dom';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDUosIzZpqS95FUvvsX2SlB5TIcxMO-63A',
+  authDomain: 'karabo-ab4c7.firebaseapp.com',
+  projectId: 'karabo-ab4c7',
+  storageBucket: 'karabo-ab4c7.appspot.com',
+  messagingSenderId: '274048737674',
+  appId: '1:274048737674:web:126b7f8697da0eb6752873',
+  measurementId: 'G-G7L50L5VVF'
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+//const fb=firebase.initializeApp
+//(firebaseConfig)
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: 'gray',
+    color: 'black'
+  }
+});
+
+function GridStyled() {
+  let history = useHistory();
+
+  const classes = useStyles();
+  return <Grid className={classes.root} />;
+}
+
+
+function App() {
+  const classes = useStyles();
+  //let history = useHistory();
+  console.log();
+  return (
+    
+    <div style={{ backgroundColor: 'turquoise' }}>
+      
+      <Grid container spacing={0}>
+        <Grid item xs={1} />
+        <Grid item xs={5} style={{ marginTop: '420px', marginBottom: '420px' }}>
+          <img
+            src="https://scontent.fjnb11-1.fna.fbcdn.net/v/t1.6435-9/107070798_899383087210144_631496564149069433_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=PXulVd66UsgAX-kOTkv&_nc_ht=scontent.fjnb11-1.fna&oh=d7052661a5449b44b6f11f7ae604d90b&oe=60FA6FDB"
+            width="100%"
+          />
+        </Grid>
+        <Grid
+          item
+          xs={5}
+          className={classes.root}
+          style={{
+            textAlign: 'center',
+            backgroundColor: 'white',
+            marginTop: '420px',
+            marginBottom: '420px'
+          }}
+        >
+          <marquee>
+            {' '}
+            <h2>WeThinkCode</h2>
+          </marquee>
+          <h3>sign up to your account</h3>
+          <TextField
+            id="outlined-basic"
+            label="Your email"
+            variant="outlined"
+          />
+          <br />
+          <TextField id="outlined-basic" label="Your name" variant="outlined" />
+          <br />
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+          <br />
+          By signing up you confirm that you've read and accepted our user
+          Notice and Privacy
+          <br />
+          <Button
+            onClick={() => {
+              firebase
+                .auth()
+                .createUserWithEmailAndPassword('malehu@gmail.com', '123456')
+                .then(userCredential => {
+                  // Signed in
+                  var user = userCredential.user;
+                  console.log(user);
+                  // ...
+                })
+                .catch(error => {
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  console.log(error.message);
+                  // ..
+                });
+            }}
+          >
+            Register
+          </Button>
+          <br />
+          OR
+          <br />
+          Alredy have an account?
+          <div>
+            {/*<Router>
+                <Link to="/todolist">
+                 
+                    
+                    Login
+                  
+                </Link>
+                <Switch>
+                  <Route exact path="/todolist" component={() =><ToDoList authorized ={true} />}/>
+
+                  <ToDoList />
+                </Switch>
+              </Router>*/}
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+export default App;
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <div> 
+  <h2>Welcome Home</h2>
+
+<button onClick={()=>{  firebase.auth().createUserWithEmailAndPassword("kamo@gmail.com", "123456")
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(error.message)
+    // ..
+  });}}>New User</button>
+
+
+
+<button onClick={()=>{  firebase.auth().signInWithEmailAndPassword("kamo@gmail.com", "123456")
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(error.message)
+    // ..
+  });}}>Sign In</button>
+
+
+
+</div>
+  
+}
+
+
+
+function About() {
+  return <h2>What is this aboutAbout</h2>;
+}
+
+function Users() {
+  return <h2>My name is function</h2>;
+}
+
+/*<Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */
+
+/*  <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <div> 
+  <h2>Welcome Home</h2>
+
+<button onClick={()=>{  firebase.auth().createUserWithEmailAndPassword("kamo@gmail.com", "123456")
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(error.message)
+    // ..
+  });}}>New User</button>
+
+
+
+<button onClick={()=>{  firebase.auth().signInWithEmailAndPassword("kamo@gmail.com", "123456")
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(error.message)
+    // ..
+  });}}>Sign In</button>
+
+
+
+</div>
+  
+}
+
+
+
+function About() {
+  return <h2>What is this aboutAbout</h2>;
+}*/
