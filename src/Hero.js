263c305253fcd83b1f818201a18b95ef25ad923c
Kaybee-debug <karabomolepo29@gmail.com>
2021-07-31 21:32:01 +0200

    3 files modified

--- src/Hero.js
+++ src/Hero.js
@@ -1,87 +1,118 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-import Search from "@material-ui/icons/Search";
-import WbSunny from "@material-ui/icons/WbSunny";
-import Star from "@material-ui/icons/Star";
-import CalendarToday from "@material-ui/icons/CalendarToday";
-import Person from "@material-ui/icons/Person";
+import Search from '@material-ui/icons/Search';
+import WbSunny from '@material-ui/icons/WbSunny';
+import Star from '@material-ui/icons/Star';
+import CalendarToday from '@material-ui/icons/CalendarToday';
+import Person from '@material-ui/icons/Person';
 //import Add from "@material-ui/icons/Add";
-import AddAlert from "@material-ui/icons/AddAlert";
-import EventAvailable from "@material-ui/icons/EventAvailable";
+import AddAlert from '@material-ui/icons/AddAlert';
+import EventAvailable from '@material-ui/icons/EventAvailable';
 import TodoList from './Componets/TodoList';
 
- const Hero = ({handleLogout}) => {
-   return(
+const Hero = ({ handleLogout }) => {
+  return (
     <section>
-    <nav>
-     
-<Grid item xs ={12} container spacing={0} >
-          
-          <Grid item xs={2} style={{backgroundColor:'white',textAlign:'center' , marginLeft:'120px', height:'500px' ,marginTop: '420px', marginBottom: '420px'}} >
+      <Grid item xs={12} container spacing={0}>
+        <Grid
+          item
+          xs={2}
+          style={{
+            backgroundColor: 'white',
+            textAlign: 'center',
+            marginLeft: '120px',
+            height: '500px',
+            marginTop: '90px'
+          }}
+        >
+          <TextField id="outlined-basic" label="Search" variant="outlined">
+            {' '}
+            <Search />
+            Search{' '}
+          </TextField>
+          <br />
+          <br />
+          <br />
+          <br />
+          <WbSunny /> My Day
+          <br />
+          <br />
+          <br />
+          <br />
+          <Star />
+          Important
+          <br />
+          <br />
+          <br />
+          <br />
+          <CalendarToday /> Planned
+          <br />
+          <br />
+          <br />
+          <br />
+          <Person /> Assigned to you <br />
+        </Grid>
+        <Grid
+          item
+          xs={5}
+          style={{
+            height: '500px',
+            marginTop: '90px',
+            overflowY: 'scroll',
+            background: '#3d7bdf'
+          }}
+        >
+          <div
+            className="todo-app"
+            style={{ marginBottom: '0px', marginTop: '0px' }}
+          >
+            <TodoList />
 
-          <TextField id="outlined-basic" label="Search" variant="outlined"> <Search/>Search </TextField>
-       <br/><br/>
-       <br/><br/>
-         <WbSunny/> My Day 
-         <br/><br/>
-         <br/><br/>
-          
-           <Star/>Important 
-          <br/><br/>
-          <br/><br/>
-          <CalendarToday/> Planned 
-         <br/><br/>
-         <br/><br/>
-
-          <Person/> Assigned to you <br/>
-    
-          </Grid>
-          <Grid item xs={5} style={{ height:'500px' ,marginTop: '420px', marginBottom: '420px' ,overflow:'scroll'}} >
-
-           <div className="todo-app" style={{  marginBottom: '0px',marginTop: '0px'}} >
-         
-            <TodoList  />
-          
-           </div>
-          
-          <img src="https://4.bp.blogspot.com/-NusT8_BxXvU/WigtjvJvKLI/AAAAAAABOyg/VP7N3f2xgkAOOU2LPKaVCBdTsMiDJp_EgCK4BGAYYCw/s1600/Web-design.jpg"  width="100%"
-              />
-            
-              </Grid>
-          <Grid item xs={3 }style={{backgroundColor:'white',textAlign:'center' ,height:'500px',marginTop: '420px', marginBottom: '420px' }}>
-            <br/>
-            <WbSunny/>add to My Day 
-            <br/><br/>
-           <br/><br/>
-            <AddAlert/> Remind Me 
-            <br/><br/>
-           <br/><br/>
-           <EventAvailable />  Add due Date 
-           <br/><br/>
-           <br/><br/>
-           <TextField id="outlined-basic" label="Add note" variant="outlined" />
-           <br/><br/>
-           <br/><br/>
-
-          
-        
-           <nav>
-         
-         <button onClick={handleLogout}>logout</button>
-
-         </nav>
-          </Grid>
-          </Grid>
-
-          
-       </nav>
-          </section>
-      
-
-       
-
-      
-   )
- }
- export default Hero;
+            <img
+              src="https://4.bp.blogspot.com/-NusT8_BxXvU/WigtjvJvKLI/AAAAAAABOyg/VP7N3f2xgkAOOU2LPKaVCBdTsMiDJp_EgCK4BGAYYCw/s1600/Web-design.jpg"
+              width="100%"
+            />
+          </div>
+        </Grid>
+        <Grid
+          item
+          xs={3}
+          style={{
+            backgroundColor: 'white',
+            textAlign: 'center',
+            height: '500px',
+            marginTop: '90px'
+          }}
+        >
+          <br />
+          <WbSunny />
+          add to My Day
+          <br />
+          <br />
+          <br />
+          <br />
+          <AddAlert /> Remind Me
+          <br />
+          <br />
+          <br />
+          <br />
+          <EventAvailable /> Add due Date
+          <br />
+          <br />
+          <br />
+          <br />
+          <TextField id="outlined-basic" label="Add note" variant="outlined" />
+          <br />
+          <br />
+          <br />
+          <br />
+          <nav>
+            <button onClick={handleLogout}>logout</button>
+          </nav>
+        </Grid>
+      </Grid>
+    </section>
+  );
+};
+export default Hero;
--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-const Login = (props) => {
+import './style.css';
+
+const Login = props => {
   const {
     email,
     setEmail,
@@ -12,18 +14,13 @@ const Login = (props) => {
     hasAcoount,
     setHasAccount,
     emailError,
-    passwordError,
-  }= props;
-  return(
-    <section>
-  <div>
-  <Grid container spacing={0}>
-        <Grid item xs={1} />
-        <Grid item xs={5} style={{ marginTop: '420px', marginBottom: '420px' }}>
-          <img
-            src="https://4.bp.blogspot.com/-NusT8_BxXvU/WigtjvJvKLI/AAAAAAABOyg/VP7N3f2xgkAOOU2LPKaVCBdTsMiDJp_EgCK4BGAYYCw/s1600/Web-design.jpg"
-            width="100%" 
-          />
+    passwordError
+  } = props;
+  return (
+    <div>
+      <Grid container spacing={0}>
+        <Grid className="picgrid" item xs={5} style={{ marginLeft: '150px',marginTop:'100px',height:'500px' }}>
+       
         </Grid>
         <Grid
           item
@@ -31,36 +28,81 @@ const Login = (props) => {
           style={{
             textAlign: 'center',
             backgroundColor: 'white',
-            marginTop: '420px',
-            marginBottom: '420px'
+            marginTop:'100px'
           }}
         >
-         
           <h3>Sign up to your account</h3>
-   
-    <TextField type="text" autoFocus required value={email} onChange={e => setEmail(e.target.value)} id="outlined-basic" label="Your email" variant="outlined" />
-    <p>{emailError}</p>
-    
-    <TextField type="password" required value={password} onChange={e => setPassword(e.target.value)} id="outlined-basic" label="Create Password" variant="outlined"/>
-    <p>{passwordError}</p>
-    <div>
-      {hasAcoount ? (
-        <>
-        <button onClick={ handleLogin} style={{color:'blue'}}>sign in</button>
-        <p>Dont have account ?<span onClick={()=> setHasAccount(!hasAcoount)} style={{color:'blue'}}>sign up</span> </p>
-        </>
-      ) : (
-        <>
-        
-        <button onClick={handleSingup} style={{color:'blue'}}>sign up</button>
-        <p>have an account ?<span onClick={() => setHasAccount(!hasAcoount)} style={{color:'blue'}}> sign in</span></p>
-        </>
-      )}
+          <br/>
+          <br/>
+
+
+          <TextField
+            type="text"
+            autoFocus
+            required
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            id="outlined-basic"
+            label="Your email"
+            variant="outlined"
+          />
+          <p>{emailError}</p>
+          <br/>
+          <br/>
+
+          <TextField
+            type="password"
+            required
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            id="outlined-basic"
+            label="Create Password"
+            variant="outlined"
+          />
+          <br/>
+          <br/>
+          <p>{passwordError}</p>
+          <div>
+            {hasAcoount ? (
+              <>
+                <button onClick={handleLogin} style={{ color: 'blue' }}>
+                  sign in
+                </button>
+                <br/>
+          <br/>
+                <p>
+                  Dont have account ?
+                  <span
+                    onClick={() => setHasAccount(!hasAcoount)}
+                    style={{ color: 'blue' }}
+                  >
+                    sign up
+                  </span>{' '}
+                </p>
+              </>
+            ) : (
+              <>
+                <button onClick={handleSingup} style={{ color: 'blue' }}>
+                  sign up
+                </button>
+                <br/>
+          <br/>
+                <p>
+                  have an account ?
+                  <span
+                    onClick={() => setHasAccount(!hasAcoount)}
+                    style={{ color: 'blue' }}
+                  >
+                    {' '}
+                    sign in
+                  </span>
+                </p>
+              </>
+            )}
+          </div>
+        </Grid>
+      </Grid>
     </div>
-    </Grid>
-    </Grid>
-  </div>
-      </section>
   );
 };
 export default Login;
