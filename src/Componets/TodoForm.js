8be5c4b32a05d980c84795589306789972830816
Kaybee-debug <karabomolepo29@gmail.com>
2021-08-03 22:06:42 +0200

    1 file modified

--- src/Componets/TodoForm.js
+++ src/Componets/TodoForm.js
@@ -1,6 +1,10 @@
 import React,{useState, useEffect, useRef} from 'react'
+import firebase from "firebase/app";
+import "firebase/firestore"
 
 function TodoForm(props){
+  const [Datalist,setData]=useState([{activity:'attend the meeting'}])
+
   const[input, setInput]= useState(props.edit ? props.edit.value: '');
   
   const inputRef= useRef(null)
@@ -50,9 +54,62 @@ className='todo-input'
 onChange={handleChange}
 ref={inputRef}
 />
-<button className='todo-button'>Add todo</button>
+<button className='todo-button'
+onClick={()=>{ 
+  console.log("writing")
+  firebase.firestore().collection('users').add({activity:'skatting'}).then(response=>{
+  console.log(response)
+})
+}}
+>Add todo</button>
 </>
 )}
+<br></br>
+<br></br>
+<br></br>
+
+<button onClick={()=>{ 
+  let val=[]
+  console.log("getting")
+  console.log(Datalist)
+  firebase.firestore().collection('users').get().then(response=>{
+  console.log(response)
+  response.forEach(data=>{
+       
+  
+    
+    val.push({...{id:data.id},...data.data()})
+     console.log(data.id);
+   
+     //setData(...Datalist,val)
+
+     
+
+   })
+setData(val)
+   //console.log("list = ",val)
+   console.log("list = ",Datalist)
+
+})
+}}>Read</button>
+  <div className = "App" >
+  <ul>
+  {Datalist.map(person => {
+    return (
+      <li key={person.id} onClick={(data)=>{
+        console.log("click",person)
+
+      }}>
+        {person.activity} 
+      </li>
+    )
+  })}
+</ul>
+     </div>
+
+
+
+  
 
 </form>
 
